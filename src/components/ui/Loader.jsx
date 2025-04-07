9fc662d3323659754977681696f9125f94debc2b
Rishab <rishabbhardwajdev@gmail.com>
2025-04-07 22:24:26 +0530

    Loader component added

---
+++ src/components/ui/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="w-18 h-18 border-8 border-gray-300 border-t-8 border-t-gray-600 rounded-full animate-spin"></div>
+  );
+};
+
+export default Loader;
